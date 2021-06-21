e9f726c5aaad99498e8aef8a4562ffee876954e9
Alejandro Hernandez <alejandrojaez@outlook.com>
2021-06-21 18:40:50 -0500

    convert app to microservice

--- apps/project-management/src/main.ts
+++ apps/project-management/src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ProjectManagementModule } from './project-management.module';
 
 async function bootstrap() {
+
   const app = await NestFactory.create(ProjectManagementModule);
-  await app.listen(3000);
+  const microservice = app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: 3001
+    }
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Auth microservice')
+    .setDescription('The auth API description')
+    .setVersion('1.0')
+    .addTag('auth')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.startAllMicroservicesAsync();
+  await app.listen(3003);
 }
 bootstrap();
