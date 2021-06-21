420635d52fa5356457fd1caf20e410a4bb094a6a
OskR219 <oeyh_219@hotmail.com>
2021-06-21 02:16:37 -0500

    Added docker for all quvels

--- apps/quvels/src/main.ts
+++ apps/quvels/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Quvels')
+    .setDescription('A scalable project for all needs.')
+    .setVersion('1.0')
+    .addTag('quvels')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
